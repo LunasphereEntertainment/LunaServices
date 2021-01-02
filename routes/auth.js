0cc901054fefb6fe81cbff6060cb0b325a7634a5
Joshua Richardson-Noyes <admiraljrn@gmail.com>
2021-01-02 13:27:29 +0000

    Tweaked authentication methods and mechanisms, to auto rollover legacy users and upgrade them to newer security accounts. After a set date, this feature will disable itself and any legacy users will require a password reset.

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,16 @@
-const crypto = require("crypto");
 const argon2 = require("argon2");
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
+const rateLimit = require("express-rate-limit");
 
-const { genSalt } = require("../util");
+const {genSalt, legacyAuth} = require("../util");
+
+// Rate Limit - Do not permit more than...
+router.use(rateLimit({
+    max: 10, // 10 requests
+    windowMs: 30 * 60 * 1000 // in a 30 minute window
+}));
 
 router.post('/login', (req, res) => {
     let knex = req.app.get('db');
@@ -17,24 +23,60 @@ router.post('/login', (req, res) => {
             if (user) {
                 let salt = user.salt;
 
-                argon2.verify(user.password, `${req.body.password}${salt}`).then((result) => {
-                    if (result) {
-                        res.json({
-                            username: req.body.username,
-                            token: jwt.sign({
-                                userId: user.user_id
-                            }, config.security.secret)
-                        });
+                let now = new Date();
+                if (user.password.contains("$")) {
+                    argon2.verify(user.password, `${req.body.password}${salt}`).then((result) => {
+                        if (result) {
+                            res.json({
+                                username: req.body.username,
+                                token: jwt.sign({
+                                    userId: user.user_id
+                                }, config.security.secret)
+                            });
+                        } else {
+                            res.status(403)
+                                .send("Username/password is incorrect. Please try again.");
+                        }
+                    });
+                } else {
+                    if (now.getTime() <= 1614556800000) {
+                        if (legacyAuth(req.body.password, user.salt, user.password)) {
+                            // Perform security upgrade for this account...
+                            let newSalt = genSalt(12);
+
+                            argon2.hash(`${req.body.password}${newSalt}`).then((hash) => {
+                                knex("users")
+                                    .update({
+                                        password: hash,
+                                        salt: newSalt
+                                    })
+                                    .where({
+                                        user_id: user.user_id
+                                    })
+                                    .then(() => {
+                                        res.json({
+                                            username: user.username,
+                                            token: jwt.sign({
+                                                userId: user.user_id
+                                            }, config.security.secret)
+                                        })
+                                    })
+                            })
+                        } else {
+                            res.status(403)
+                                .send("Username/password is incorrect. Please try again.");
+                        }
                     } else {
+                        // This account may need upgrading.
                         res.status(403);
-                        res.send("Username/password is incorrect. Please try again");
+                        res.send("Username/password is incorrect. Please try again.\nYour account may have been deactivated due to inactivity. If so, proceed with resetting your password below.");
                     }
-                })
+                }
             } else {
                 res.status(404);
                 res.send("User account does not exist.");
             }
-        })
+        });
 });
 
 router.post('/register', (req, res) => {
--- util.js
+++ util.js
@@ -1,9 +1,14 @@
-const { randomBytes } = require("crypto");
+const { randomBytes, createHmac } = require("crypto");
 
 module.exports = {
     genSalt: function(length){
         return randomBytes(Math.ceil(length/2))
             .toString('hex') /** convert to hexadecimal format */
             .slice(0,length);   /** return required number of characters */
+    },
+    legacyAuth: function(pass, salt, hash) {
+        let newHash = createHmac('sha512', salt).update(pass).digest('hex');
+
+        return newHash === hash;
     }
 }
